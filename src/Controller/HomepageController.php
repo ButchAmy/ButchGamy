a671ca386daee0ad0c88b88c9ad30b1779412386
ButchAmy <arne.dhollander@gmail.com>
2024-02-11 17:35:36 +0000

    Added game list to homepage. site seems wonky though

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,19 +2,20 @@
 
 namespace App\Controller;
 
+use App\Repository\GameRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomepageController extends AbstractController
 {
-
-	#[Route('/', name: 'app_index')]
-    #[Route('/homepage', name: 'app_homepage')]
-    public function index(): Response
-    {
-        return $this->render('homepage/index.html.twig', [
-            'controller_name' => 'HomepageController',
-        ]);
+	#[Route('/', name: 'app_index', methods: ['GET'])]
+    #[Route('/homepage', name: 'app_homepage', methods: ['GET'])]
+	#[Route('/homepage/all', name: 'app_homepage_all', methods: ['GET'])]
+	public function index(GameRepository $gameRepository): Response
+	{
+		return $this->render('homepage/index_all.html.twig', [
+			'games' => $gameRepository->findBy(['public' => true]),
+		]);
     }
 }
